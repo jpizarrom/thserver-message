c8935b8cd98dcea8d729748f4911b801359b10a2
Juan Pizarro <jpizarro@doingit.cl>
2011-02-14 10:16:09 -0300

    url rest

---
+++ thlib-msg/src/main/java/com/jpizarro/th/lib/message/util/MessageRestURL.java
@@ -0,0 +1,10 @@
+package com.jpizarro.th.lib.message.util;
+
+
+public class MessageRestURL {
+
+	public static final String ENTITY = "messages";
+	
+	public static final String ENTITY_ID = "/{id}";
+
+}
--- thserver-msg/src/main/java/com/jpizarro/th/server/message/view/rest/MessageController.java
+++ thserver-msg/src/main/java/com/jpizarro/th/server/message/view/rest/MessageController.java
@@ -3,6 +3,7 @@ package com.jpizarro.th.server.message.view.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,23 +11,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jpizarro.th.lib.message.entity.MessageTO;
+import com.jpizarro.th.lib.message.util.MessageRestURL;
 import com.jpizarro.th.server.generic.model.persistence.util.exceptions.DuplicateInstanceException;
 import com.jpizarro.th.server.generic.model.persistence.util.exceptions.InstanceNotFoundException;
+import com.jpizarro.th.server.generic.util.ResourceNotFoundException;
 import com.jpizarro.th.server.generic.view.rest.GenericController;
 import com.jpizarro.th.server.message.model.service.MessageService;
 
 @Controller
-@RequestMapping("/messages")
+@RequestMapping(MessageRestURL.ENTITY)
 public class MessageController implements GenericController<MessageTO, Long>{
 	@Autowired
 	private MessageService messageService;
 
 	private String XML_VIEW_NAME = "xmlView";
 	
-	@RequestMapping(method=RequestMethod.GET, value="/{id}")
+	@RequestMapping(method=RequestMethod.GET, value=MessageRestURL.ENTITY_ID)
 	@ResponseBody
 	public MessageTO getEntity(@PathVariable Long id) {
 		MessageTO to = null;
@@ -35,9 +39,11 @@ public class MessageController implements GenericController<MessageTO, Long>{
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new ResourceNotFoundException();
 		} catch (InstanceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new ResourceNotFoundException();
 		}
 		return to;
 //		return new ModelAndView(XML_VIEW_NAME, BindingResult.MODEL_KEY_PREFIX+"team", to);
@@ -51,6 +57,7 @@ public class MessageController implements GenericController<MessageTO, Long>{
 
 	@RequestMapping(method=RequestMethod.POST)
 	@ResponseBody
+	@ResponseStatus(value=HttpStatus.CREATED)
 	public MessageTO addEntity(@RequestBody MessageTO body) {
 		MessageTO msg = null;
 		try {
@@ -64,7 +71,7 @@ public class MessageController implements GenericController<MessageTO, Long>{
 	}
 
 	@Override
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	@RequestMapping(value=MessageRestURL.ENTITY_ID, method=RequestMethod.PUT)
 	@ResponseBody
 	public MessageTO updateEntity(@PathVariable Long id, @RequestBody MessageTO entity) {
 		entity.setMessageId(id);
@@ -73,12 +80,13 @@ public class MessageController implements GenericController<MessageTO, Long>{
 		} catch (InstanceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new ResourceNotFoundException();
 		}
-		return null;
+//		return null;
 	}
 
 	@Override
-	@RequestMapping(method=RequestMethod.DELETE, value="/{id}")
+	@RequestMapping(method=RequestMethod.DELETE, value=MessageRestURL.ENTITY_ID)
 	public ModelAndView removeEntity(@PathVariable Long id) {
 		boolean ret = true;
 		try {
@@ -86,6 +94,7 @@ public class MessageController implements GenericController<MessageTO, Long>{
 		} catch (InstanceNotFoundException e) {
 			e.printStackTrace();
 			ret = false;
+			throw new ResourceNotFoundException();
 		}
 		return new ModelAndView(XML_VIEW_NAME, BindingResult.MODEL_KEY_PREFIX+"team", ret);
 	}
